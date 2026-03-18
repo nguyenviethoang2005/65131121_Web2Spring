0651ee83aefbcc49fb107e370bf3fe8640023f3a
Nguyễn Việt Hoàng <151610892+nguyenviethoang2005@users.noreply.github.com>
2026-03-18 10:46:26 +0700

    Update

--- BaiTH3/src/main/java/hoang/thSpringBoot/controllers/B2_TruyenDuLieuSangView.java
+++ BaiTH3/src/main/java/hoang/thSpringBoot/controllers/B2_TruyenDuLieuSangView.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import julian.thSpringBoot.models.SinhVien;
+import hoang.thSpringBoot.models.SinhVien;
 
 @Controller
 public class B2_TruyenDuLieuSangView {
